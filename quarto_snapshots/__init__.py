36a86991262bd5319c8a6b416acfb77c91820417
Nikolas Siccha <nikolassiccha@gmail.com>
2023-05-16 10:52:45 +0300

    try to expose main

--- quarto_snapshots/__init__.py
+++ quarto_snapshots/__init__.py
@@ -3,3 +3,13 @@
 __author__ = """Nikolas Siccha"""
 __email__ = 'nikolassiccha@gmail.com'
 __version__ = '0.1.0'
+
+from .quarto_snapshots import *
+
+__all__ = [
+    "find_and_copy_snapshots",
+    "generate",
+    "get_parser",
+    "handle_args",
+    "main"
+]
