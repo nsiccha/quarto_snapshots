5640829c64abbdd5924caa44fec0ca6722650f37
Nikolas Siccha <nikolassiccha@gmail.com>
2023-05-23 13:45:53 +0300

    added ipynb support

--- quarto_snapshots/quarto_snapshots.py
+++ quarto_snapshots/quarto_snapshots.py
@@ -1,6 +1,6 @@
 """Main module."""
 import sys
-import os, argparse, pathlib, re, shlex
+import os, argparse, pathlib, re, shlex, json
 import git# comes from gitpython
 import frontmatter# comes from python-frontmatter
 
@@ -8,8 +8,32 @@ def print_and_system(cmd):
     print(cmd)
     os.system(cmd)
 
+class notebook:
+    def get(self, *args): return self.frontmatter.get(*args)
+    def __getitem__(self, key): return self.frontmatter.__getitem__(key)
+    def __setitem__(self, *args): return self.frontmatter.__setitem__(*args)
+
+class raw_notebook(notebook):
+    def __init__(self, content):
+        self.content = content
+        self.frontmatter = frontmatter.loads(content)
+    def dump(self, path): frontmatter.dump(self.content, path)
+
+class ipynb_notebook(notebook):
+    def __init__(self, content):
+        self.content = content
+        self.json = json.loads(content)
+        self.frontmatter = frontmatter.loads("".join(self.json["cells"][0]["source"]))
+    def dump(self, path):
+        self.json["cells"][0]["source"] = frontmatter.dumps(self.frontmatter).split("\n")
+        json.dump(self.json, path)
+
+def get_notebook(content, suffix):
+    return ipynb_notebook(content) if suffix == ".ipynb" else raw_notebook(content)
+
 def find_and_copy_snapshots(args, path):
     # https://stackoverflow.com/questions/28803626/get-all-revisions-for-a-specific-file-in-gitpython
+    suffix = path.suffix
     commits = list(args.repo.iter_commits(paths=[path]))
     versions = dict()
     contents = [
@@ -18,31 +42,34 @@ def find_and_copy_snapshots(args, path):
     ]
     with open(path, "r") as fd: versions['latest'] = fd.read()
     for content in contents: 
-        version = frontmatter.loads(content).get("version", "unversioned")
-        versions[version] = content
+        nb = get_notebook(content, suffix)
+        version = nb.get("version", "unversioned")
+        versions[version] = nb
     if not args.keep_unversioned: versions.pop("unversioned", None) 
-    for version, content in versions.items():
+    for version, nb in versions.items():
         stem = "index" if version == "latest" else f"{path.stem}_{version}"
-        suffix = path.suffix
         rel_path = path.relative_to(args.quarto_project).with_suffix("")
         if path.stem == "index": rel_path = rel_path.parent
         snapshot_path = args.snapshots_dir / rel_path / f"{stem}{suffix}"
         snapshot_path.parent.mkdir(parents=True, exist_ok=True)
         print(f"Generating {snapshot_path}...")
-        modified_content = frontmatter.loads(content)
-        modified_title = modified_content.get("title", path.stem)
+        modified_title = nb.get("title", path.stem)
         if version != "latest": modified_title += f" ({version})"
         if path == args.quarto_project / f"index{suffix}": 
             modified_title = "SNAPSHOTS"
-            modified_content["order"] = 10 + modified_content.get("order", 0)
-        modified_content["title"] = modified_title
-        frontmatter.dump(modified_content, snapshot_path)
+            nb["order"] = 10 + nb.get("order", 0)
+        nb["title"] = modified_title
+        nb.dump(snapshot_path)
 
 def generate(args):
     args.repo = git.Repo(args.git_root)
     args.quarto_project = pathlib.Path(args.quarto_project)
     args.snapshots_dir = args.quarto_project / args.snapshots_subdir
-    for path in args.quarto_project.rglob("*.*md"):
+    paths = sum([
+        list(args.quarto_project.rglob(f"*.*{ext}")) 
+        for ext in ["md", "qmd", "ipynb"] 
+    ], [])
+    for path in paths:
         try:
             path.relative_to(args.snapshots_dir)
         except:
